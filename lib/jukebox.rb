6eaafb500262d90fcbb489bf547e87e587ab53b3
Nathan Bell <nathanbell09@gmail.com>
2018-11-11 14:43:53 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,7 +25,7 @@ end
 def play(songs)
   p "Please enter a song name or number:"
   user_input = gets.chomp
-  songs.each_with_index do |song, index|
+  p songs.each_with_index do |song, index|
     if user_input == song || user_input == (index + 1).to_s
       return "Playing #{song}"
     else
